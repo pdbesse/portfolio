8d43dc61b92f6cd6fe6755878760737ccac7cc50
Phil-Laptop <phillip.besse@gmail.com>
2022-10-29 10:17:57 -0400

    update Subject placeholder

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -93,7 +93,7 @@ const ContactForm = ({ setAlertContent, setShowAlert }) => {
             </Form.Label>
             <Form.Control className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
               type="text"
-              placeholder="name@example.com"
+              placeholder="Subject"
               onChange={handleChange}
               value={formData.subject}
             />
